8bc9d5f1c95ca1a1d23451b02243b1f68fa8c76d
Indrajeet Mondal <indrajeetmondal120703@gmail.com>
2024-07-17 21:30:33 +0530

    Create Program6.py
    Learnt about python functions basics

---
+++ A2Z DSA/Learn the basics/Program6.py
@@ -0,0 +1,13 @@
+# Functions basics
+# Name = Indrajeet Mondal; Date = 17th July 2024
+# SourceCode
+
+def addTwoNum(a, b):
+    return a+b
+
+x = int(input("Enter a number:- "))
+y = int(input("Enter another number:- "))
+
+sum_of_two = addTwoNum(x, y)
+
+print(f"The sum of the two numbers is {sum_of_two}.")
