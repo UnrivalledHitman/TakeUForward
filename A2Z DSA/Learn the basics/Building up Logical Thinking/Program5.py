a18a4e63d450e34c9b0b3a360faf29f4a5fd40dc
Indrajeet Mondal <indrajeetmondal120703@gmail.com>
2024-07-18 10:59:33 +0530

    Pattern Question 5
    Program to print left aligned inverted pyramid

---
+++ A2Z DSA/Learn the basics/Building up Logical Thinking/Program5.py
@@ -0,0 +1,10 @@
+# Program to print left aligned inverted pyramid
+# Name = Indrajeet Mondal; Date 18th July 2024
+# SourceCode
+
+x = int(input("Enter the number of rows in the pyramid:- "))
+
+for var1 in range(x, 0, -1):
+    for var2 in range(var1):
+        print("* ", end="")
+    print()
