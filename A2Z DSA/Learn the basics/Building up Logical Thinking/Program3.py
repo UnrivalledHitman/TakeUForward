2d1b371108732c399fef8d43e7d55753ed99c94f
Indrajeet Mondal <indrajeetmondal120703@gmail.com>
2024-07-18 09:01:18 +0530

    Pattern Question 3
    Program to print left aligned pyramid with numbers from 1 to the number of row

---
+++ A2Z DSA/Learn the basics/Building up Logical Thinking/Program3.py
@@ -0,0 +1,10 @@
+# Program to print left aligned pyramid with numbers from 1 to the number of row
+# Name = Indrajeet Mondal; Date = 18th July 2024
+# SourceCode
+
+x = int(input("Enter the number of rows in the pyramid:- "))
+
+for var1 in range(1, x+1):
+    for var2 in range(1, var1+1):
+        print(var2, end='')
+    print()
