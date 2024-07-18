004b49970f3527b21fc7e58e63ce738462b26a8a
Indrajeet Mondal <indrajeetmondal120703@gmail.com>
2024-07-18 12:30:37 +0530

    Update Program5.py
    Replaced double quotes with single quotes

--- A2Z DSA/Learn the basics/Building up Logical Thinking/Program5.py
+++ A2Z DSA/Learn the basics/Building up Logical Thinking/Program5.py
@@ -6,5 +6,5 @@ x = int(input("Enter the number of rows in the pyramid:- "))
 
 for var1 in range(x, 0, -1):
     for var2 in range(var1):
-        print("* ", end="")
+        print("* ", end='')
     print()
