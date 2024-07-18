588fa72612c6050e29b66711fc1595e3e5ec67dc
Indrajeet Mondal <indrajeetmondal120703@gmail.com>
2024-07-18 08:53:17 +0530

    Pattern Question 1
    Program to print a rectangle pattern

---
+++ A2Z DSA/Learn the basics/Building up Logical Thinking/Program1.py
@@ -0,0 +1,11 @@
+# Printing rectangle pattern
+# Name = Indrajeet Mondal; Date = 18th July 2024
+# SourceCode
+
+x = int(input("Enter the number of rows in the pattern:- "))
+y = int(input("Enter the number of columns in the pattern:- "))
+
+for var1 in range(x):
+    for var2 in range(y):
+        print("* ", end='')
+    print()
