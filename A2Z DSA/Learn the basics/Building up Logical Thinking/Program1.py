24f1af174a67088eca4dc2d6ce3c46f02c8a1c83
Indrajeet Mondal <indrajeetmondal120703@gmail.com>
2024-07-19 22:32:39 +0530

    Update Program1.py
    Added comments

--- A2Z DSA/Learn the basics/Building up Logical Thinking/Program1.py
+++ A2Z DSA/Learn the basics/Building up Logical Thinking/Program1.py
@@ -5,7 +5,9 @@
 x = int(input("Enter the number of rows in the pattern:- "))
 y = int(input("Enter the number of columns in the pattern:- "))
 
+# Outer loop for no. of rows
 for var1 in range(x):
+    # Inner loop for no. of columns
     for var2 in range(y):
         print("* ", end="")
     print()
