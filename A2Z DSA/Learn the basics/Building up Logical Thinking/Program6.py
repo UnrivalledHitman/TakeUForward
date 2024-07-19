bfd6eb9d3f81f39ab6f012175a8ed6576cd17bcc
Indrajeet Mondal <indrajeetmondal120703@gmail.com>
2024-07-19 22:45:50 +0530

    Update Program6.py
    Added Comments

--- A2Z DSA/Learn the basics/Building up Logical Thinking/Program6.py
+++ A2Z DSA/Learn the basics/Building up Logical Thinking/Program6.py
@@ -3,7 +3,10 @@
 # SourceCode
 
 x = int(input("Enter the number of rows in the pyramid:- "))
+
+# Outer loop for height of the pyramid
 for var1 in range(x, 0, -1):
+    # Inner loop for rows
     for var2 in range(1, var1 + 1):
         print(var2, end='')
     print()
