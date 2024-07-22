8aefe70b65b0158648acc76ad6d47bee6a420360
Indrajeet Mondal <indrajeetmondal120703@gmail.com>
2024-07-22 22:27:06 +0530

    Pattern Question 16
    Print left aligned pyramid pattern with repetitive capital letters

---
+++ A2Z DSA/Learn the basics/Building up Logical Thinking/Program16.py
@@ -0,0 +1,23 @@
+# Program to print left aligned pyramid pattern with repetitive capital letters
+# Name = Indrajeet Mondal; Date = 22nd July 2024
+# SourceCode
+
+
+# Function to convert number to character
+def convert_to_char(a):
+    return chr(a)
+
+
+x = int(input("Enter height of the pyramid pattern:- "))
+# 65 is the ASCII code for 'A'
+y = 65
+
+# Outer loop for no. of rows
+for var1 in range(1, x + 1):
+
+    # Inner loop printing the alphabets
+    for var2 in range(var1):
+        char = convert_to_char(y)
+        print(f"{char} ", end="")
+    y += 1
+    print()
