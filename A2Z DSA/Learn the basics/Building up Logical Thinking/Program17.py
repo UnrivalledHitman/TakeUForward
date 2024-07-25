99e5be195f27c57b66682ebdd8f7a3d132e902f1
Indrajeet Mondal <indrajeetmondal120703@gmail.com>
2024-07-26 01:36:13 +0530

    Update Program17.py
    Added the ord() fn to use char and int interchanging and replaced break_point division from / to //, i.e from normal division to floor division.

--- A2Z DSA/Learn the basics/Building up Logical Thinking/Program17.py
+++ A2Z DSA/Learn the basics/Building up Logical Thinking/Program17.py
@@ -2,35 +2,25 @@
 # Name = Indrajeet Mondal; Date = 25th July 2024
 # SourceCode
 
-
-# Function to convert number to character
-def convert_to_char(a):
-    return chr(a)
-
-
 x = int(input("Enter height of the pyramid pattern:- "))
 
-# Outer loop for height of the pyramid
-for var1 in range(1, x + 1):
-    # To print left side spaces
-    for var2 in range(x - var1):
+# Outer loop for the number of rows.
+for i in range(x):
+    # For printing the left side spaces.
+    for j in range(x - i - 1):
         print(" ", end="")
-    # To print the capital letters
-    # 65 is the ASCII code for 'A'
-    y = 65
-    # Increasing order of capital letters print
-    for var3 in range((2 * var1 - 1)):
-        print_char = convert_to_char(y)
-        print(f"{print_char}", end="")
-        y += 1
-        z = y
-    z -= 1
-    # Decreasing order of capital letters print
-    for var4 in range(2 * var1):
-        print_char = convert_to_char(z)
-        print(f"{print_char}", end="")
-        z -= 1
-    # To print the right side spaces
-    for var5 in range(x - var1):
+    # For printing the characters.
+    ch = "A"
+    break_point = (2 * i + 1) // 2
+    for j in range(1, 2 * i + 1 + 1):
+        print(ch, end="")
+        if j <= break_point:
+            # ord() gives the unicode of the character passed as argument
+            ch = chr(ord(ch) + 1)
+        else:
+            ch = chr(ord(ch) - 1)
+    # for printing the right side spaces
+    for j in range(x - i - 1):
         print(" ", end="")
     print()
+
