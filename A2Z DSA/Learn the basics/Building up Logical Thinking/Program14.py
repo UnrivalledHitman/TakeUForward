c605409b6c4f8787ba32609a2f99b3b6cc2de84e
Indrajeet Mondal <indrajeetmondal120703@gmail.com>
2024-07-26 13:14:18 +0530

    Update Program14.py
    Removed the function to convert to char explicitly, and used int to chr conversion

--- A2Z DSA/Learn the basics/Building up Logical Thinking/Program14.py
+++ A2Z DSA/Learn the basics/Building up Logical Thinking/Program14.py
@@ -2,21 +2,13 @@
 # Name = Indrajeet Mondal; Date = 22nd July 2024
 # SourceCode
 
+x = int(input("Enter the height of the pyramid:- "))
 
-# Function to convert number to character
-def convert_to_char(a):
-    return chr(a)
-
-
-x = int(input("Enter height of the pyramid pattern:- "))
-
-# Outer loop for no. of rows
-for var1 in range(1, x + 1):
-    # 65 is the ASCII code for 'A'
-    y = 65
-    # Inner loop printing the alphabets
-    for var2 in range(var1):
-        char = convert_to_char(y)
-        print(f"{char} ", end="")
-        y += 1
+# Outer loop for height of the pyramid
+for i in range(1, x + 1):
+    # ASCII code for 'A'
+    print_char = ord("A")
+    for j in range(i):
+        print(chr(print_char), end="")
+        print_char += 1
     print()
