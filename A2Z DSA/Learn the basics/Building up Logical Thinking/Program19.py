943832f36f453e4eaa164e306a149eb4cb9e83ef
Indrajeet Mondal <indrajeetmondal120703@gmail.com>
2024-07-27 07:42:53 +0530

    Pattern Question 19
    Print hollow diamond within rectangle pattern

---
+++ A2Z DSA/Learn the basics/Building up Logical Thinking/Program19.py
@@ -0,0 +1,32 @@
+# Print hollow diamond within rectangle pattern
+# Name = Indrajeet Mondal; Date = 27th July 2024
+# SourceCode
+
+x = int(input("Enter height of the pattern:- "))
+
+# Printing the top half
+# Outer loop for the number of rows
+for var1 in range(x):
+    # Inner loop 1 to print left white spaces
+    for var2 in range(x - var1):
+        print("*", end="")
+    # Inner loop 2 to print the star
+    for var3 in range(2 * var1 + 1):
+        print(" ", end="")
+    # Inner loop 3 to print the right white spaces
+    for var4 in range(x - var1):
+        print("*", end="")
+    print()
+
+# Printing the lower half
+for var1 in range(x):
+    # Inner loop 1 to print left white spaces
+    for var2 in range(var1 + 1):
+        print("*", end="")
+    # Inner loop 2 to print the star
+    for var3 in range(2 * x - (2 * var1 + 1)):
+        print(" ", end="")
+    # Inner loop 3 to print the right white spaces
+    for var4 in range(var1 + 1):
+        print("*", end="")
+    print()
