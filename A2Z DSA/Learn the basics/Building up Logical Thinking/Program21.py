48550f4f04ab191c4ec90f1be9a4dd7171b7120a
Indrajeet Mondal <indrajeetmondal120703@gmail.com>
2024-07-27 08:19:06 +0530

    Pattern Question 21
    Print a hollow square pattern

---
+++ A2Z DSA/Learn the basics/Building up Logical Thinking/Program21.py
@@ -0,0 +1,17 @@
+# Print a hollow square pattern
+# Name = Indrajeet Mondal; Date = 27th July 2024
+# SourceCode
+
+x = int(input("Enter side length of the square pattern:- "))
+
+# Outer loop for height of the pattern
+for i in range(x):
+    # Inner loop for row content
+    for j in range(x):
+        # Printing star condition
+        if i == 0 or i == x - 1 or j == 0 or j == x - 1:
+            print("* ", end="")
+        # Printing space condition
+        else:
+            print("  ", end="")
+    print()
