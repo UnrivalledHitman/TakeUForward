f17f69ceeae01d9e3774a541177dd12a529c37c9
Indrajeet Mondal <indrajeetmondal120703@gmail.com>
2024-07-27 07:45:39 +0530

    Update Program19.py
    Updated the comments

--- A2Z DSA/Learn the basics/Building up Logical Thinking/Program19.py
+++ A2Z DSA/Learn the basics/Building up Logical Thinking/Program19.py
@@ -7,26 +7,26 @@ x = int(input("Enter height of the pattern:- "))
 # Printing the top half
 # Outer loop for the number of rows
 for var1 in range(x):
-    # Inner loop 1 to print left white spaces
+    # Inner loop 1 to print left side stars
     for var2 in range(x - var1):
         print("*", end="")
-    # Inner loop 2 to print the star
+    # Inner loop 2 to print center spaces
     for var3 in range(2 * var1 + 1):
         print(" ", end="")
-    # Inner loop 3 to print the right white spaces
+    # Inner loop 3 to print the right side stars
     for var4 in range(x - var1):
         print("*", end="")
     print()
 
 # Printing the lower half
 for var1 in range(x):
-    # Inner loop 1 to print left white spaces
+    # Inner loop 1 to print left side stars
     for var2 in range(var1 + 1):
         print("*", end="")
-    # Inner loop 2 to print the star
+    # Inner loop 2 to print center spaces
     for var3 in range(2 * x - (2 * var1 + 1)):
         print(" ", end="")
-    # Inner loop 3 to print the right white spaces
+    # Inner loop 3 to print the right side stars
     for var4 in range(var1 + 1):
         print("*", end="")
     print()
