679355d3b0f066a206df002e8dbe73e97b2a5028
Indrajeet Mondal <indrajeetmondal120703@gmail.com>
2024-07-31 14:14:06 +0530

    Create Program4.py
    Reverse the given number (Optimal Approach)

---
+++ A2Z DSA/Learn the basics/Know Basic Maths/Program4.py
@@ -0,0 +1,15 @@
+# Reverse the given number (Optimal Approach)
+# Name = Indrajeet Mondal; Date = 31st July 2024
+# SourceCode
+
+x = int(input("Enter a number:- "))
+z = x
+digits = []
+
+# Extracting all the digits as well as printing in reverse order
+while z > 0:
+    if z % 10 == 0:
+        z = z // 10
+    else:
+        print(z % 10, end="")
+        z = z // 10
