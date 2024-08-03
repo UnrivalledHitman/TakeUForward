5afc5fa472a286780de4f25fce536fe286f72c28
Indrajeet Mondal <indrajeetmondal120703@gmail.com>
2024-08-03 20:39:39 +0530

    Create Program7.py
    Reverse an array

---
+++ A2Z DSA/Learn the basics/Recursion/Program7.py
@@ -0,0 +1,15 @@
+# Reverse an array
+# Name = Indrajeet Mondal; Date = 3rd August 2024
+# SourceCode
+
+arr_len = int(input("Enter the length of the input array: "))
+arr = []
+
+for i in range(arr_len):
+    x = int(input("Enter a number: "))
+    arr.append(x)
+
+print(f"Original array is: {arr}")
+
+arr.reverse()
+print(f"Reversed array is: {arr}")
